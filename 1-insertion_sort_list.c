230368f50c353711508231d5a43e5d1fac529f38
Tunde <tundeogunmodede1@gmail.com>
2024-02-19 02:57:58 -0800

    new fix

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * insert_sort_list - sorts a doubly linked list
+ * @list: pointer to the head
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *tmp;
+
+	if (!list || !(*list) || !((*list)->next))
+		return;
+
+	current = (*list)->next;
+	
+	while (current)
+	{
+		tmp = current;
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
