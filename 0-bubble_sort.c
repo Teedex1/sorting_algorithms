868f3d6c7fad923be39b7c13384c8a5b4fa72d8b
Tunde <tundeogunmodede1@gmail.com>
2024-02-19 02:35:11 -0800

    new fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,15 +10,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp, sort;
+	int tmp;
 
-	if (!array || size == 0)
+	if (size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		sort = 0;
-
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,12 +24,36 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				sort = 1;
+				print_array(array, size);
 			}
 		}
-		if (!sort)
-			break;
-
-		print_array(array, size);
 	}
+
+	/*{
+	* size_t i, j;
+	*int sort, tmp;
+
+	*if (!array || size == 0)
+	*	return;
+
+*	for (i = 0; i < size - 1; i++)
+*	{
+*		sort = 0;
+*
+*		for (j = 0; j < size - i - 1; j++)
+*		{
+*			if (array[j] > array[j + 1])
+*			{
+*				tmp = array[j];
+*				array[j] = array[j + 1];
+*				array[j + 1] = tmp;
+*				sort = 1;
+*			}
+*		}
+*		print_array(array, size);
+*		if (!sort)
+*			break;
+*
+*		print_array(array, size);
+*/
 }
