17d88906bfbfff0d04a5c21e46a7968ea5587678
Tunde <tundeogunmodede1@gmail.com>
2024-02-19 03:24:42 -0800

    new fix

--- sort.h
+++ sort.h
@@ -23,5 +23,5 @@ void print_list(const listint_t *list);
 /* Main Sorting Algorithms Function Tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
 #endif
